9967c4f16e1cdff6325cb9a0363156e96d4690a1
Eldan Goldenberg <eldang@gmail.com>
2016-03-25 14:49:57 -0700

    Special handling for highway numbers
    Resolves #1

--- code/parse_addresses.py
+++ code/parse_addresses.py
@@ -18,35 +18,67 @@ import unicodecsv as csv
 # https://github.com/jdunck/python-unicodecsv
 
 # address normalisation by https://github.com/openvenues/pypostal
-from postal.expand import expand_address
 from postal.parser import parse_address
 
 def main():
   print_with_timestamp("Starting run.")
   
   inputfile = "../Hennepin County Moving Violations 2010-2015.csv"
+  interstates = ["494", "694", "394", "35w", "94"]
+  us_highways = ["12", "169", "212"]
+  state_routes = ["62", "77", "100", "101", "610"]
+  directions = ["nb", "northbound", "eb", "eastbound", "sb", "southbound", "wb", "westbound"]
 
   outputfile = inputfile[:-4] + "_regularised_addresses.csv"
   with open(outputfile, 'w') as f_out:
     with open(inputfile, 'rU') as f_in:
       reader = csv.DictReader(f_in, dialect="excel")
-      writer = csv.DictWriter(f_out, reader.fieldnames + ["parsed_address", "parsed_w_city", "parsed_w_state", "parsed_w_county_and_state"], encoding="utf8")
+      writer = csv.DictWriter(f_out, reader.fieldnames + ["autoparsed", "parsed_address", "parsed_w_city", "parsed_w_state", "parsed_w_county_and_state"], encoding="utf8")
       writer.writeheader()
       n = 0
       for row in reader:
-        offloctn = row["offloctn"].lower().replace("n/b", "northbound").replace("e/b", "eastbound").replace("s/b", "southbound").replace("w/b", "westbound").replace("/", "&").replace("@", "&")
+        offloctn = row["offloctn"].lower().replace("n/b", "").replace("e/b", "").replace("s/b", "").replace("w/b", "").replace("nb ", "").replace("eb ", "").replace("sb ", "").replace("wb ", "").replace(" nb", "").replace(" eb", "").replace(" sb", "").replace(" wb", "").replace("/", "&").replace("@", "&").replace(" from ", "&").replace(" at ", "&").replace("&&", "&")
 #        print offloctn
         addr = ""
         for loc in offloctn.split("&"):
           if addr != "":
             addr = addr + " & "
-          for token in parse_address(loc + ", Hennepin County, Minnesota, USA"):
-            if token[0] not in (u'hennepin county', u'minnesota', u'usa'):
-              if token[1] in (u'suburb', u'city'):
-                addr = addr + ", "
-              addr = addr + token[0] + " "
+          found = False
+          for interstate in interstates:
+            if loc.strip() == interstate:
+              addr += "I-" + interstate
+#              print loc, "||", row["offloctn"], "||", addr
+              found = True
+              break
+          if not found:
+            for hwy in us_highways:
+              if loc.strip() == hwy:
+                addr += "US-" + hwy
+#                print loc, "||", row["offloctn"], "||", addr
+                found = True
+                break
+          if not found:
+            for rte in state_routes:
+              if loc.strip() == rte:
+                addr += "SR-" + rte
+#                print loc, "||", row["offloctn"], "||", addr
+                found = True
+                break     
+          if not found:
+            newpart = ""
+            for token in parse_address(loc + ", Hennepin County, Minnesota, USA"):
+              if token[0] not in (u'hennepin county', u'minnesota', u'usa'):
+                if newpart != "":
+                  if token[1] in (u'suburb', u'city'):
+                    newpart += ", "
+                  else:
+                    newpart += " "
+                newpart += token[0]
+                  
+            addr = addr + newpart
 #          print parse_address(loc + ", Hennepin County, Minnesota, USA")
 #          print addr
+        addr = addr.replace("cr ", "County Road").replace("co rd ", "County Road")
         row["parsed_address"] = addr
         row["parsed_w_city"] = addr + "Minneapolis, Minnesota, USA"
         row["parsed_w_state"] = addr + ", Minnesota, USA"
@@ -58,17 +90,7 @@ def main():
           print_with_timestamp("Wrote " + str(n) + " rows so far.")
 
   print_with_timestamp("Run complete.")
-
-
-
-def prettify_address(address):
-  print expand_address(address)
-  parts = parse_address(expand_address(address)[0])
-  print parts
-  prettified = ""
-  for part in parts:
-    prettified = prettified + part[0] + ", "
-  return prettified[0:-2]      
+ 
 
 
 
